7d3aa5701cdf6b97c8a6645ef907ab13c0047d4d
Diego Fabian Cancho Coila <u20201f479@upc.edu.pe>
2023-11-24 22:54:27 -0500

    Code: Implement CORS policy

--- src/main/java/com/furnigo/FurnigoApplication.java
+++ src/main/java/com/furnigo/FurnigoApplication.java
@@ -2,6 +2,9 @@ package com.furnigo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class FurnigoApplication {
@@ -9,5 +12,15 @@ public class FurnigoApplication {
     public static void main(String[] args) {
         SpringApplication.run(FurnigoApplication.class, args);
     }
-
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*");
+            }
+        };
+    }
 }
